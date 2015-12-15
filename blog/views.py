4db7ae432529c6cb4d68d468946f7f4ce5037002
@Gaston <gaetan.pecqueux@gmail.com>
2015-12-15 17:29:25 +0100

    first view
    première vue
    
    127.0.0.1:8000/accueil

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>HW</h1>
+              <p></p>"""
+    return HttpResponse(text)
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns = [
+    url(r'^accueil$', views.home),
 ]
