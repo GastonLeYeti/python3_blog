0e43c91dd68da78383432740c4723074519b35f6
@Gaston <gaetan.pecqueux@gmail.com>
2015-12-19 00:42:39 +0100

    add Article in admin
    gestion des articles via le panel admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Categorie, Article
 
-# Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+   list_display   = ('titre', 'auteur', 'date')
+   list_filter    = ('auteur','categorie',)
+   date_hierarchy = 'date'
+   ordering       = ('date', )
+   search_fields  = ('titre', 'contenu')
