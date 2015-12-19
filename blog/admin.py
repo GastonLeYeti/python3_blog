fcefde89362df2ab0efc7cf8387505e04d50ee64
@Gaston <gaetan.pecqueux@gmail.com>
2015-12-19 01:13:07 +0100

    update admin panel
    Update de la partie création/modification d’un article

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from blog.models import Categorie, Article
 
-admin.site.register(Categorie)
-admin.site.register(Article)
-
 class ArticleAdmin(admin.ModelAdmin):
-   list_display   = ('titre', 'auteur', 'date')
-   list_filter    = ('auteur','categorie',)
-   date_hierarchy = 'date'
-   ordering       = ('date', )
-   search_fields  = ('titre', 'contenu')
+
+    # Configuration de la liste d'articles
+    list_display   = ('titre', 'categorie', 'auteur', 'date')
+    list_filter    = ('auteur','categorie', )
+    ordering       = ('date', )
+    search_fields  = ('titre', 'contenu')
+
+    # Configuration du formulaire d'édition
+    fieldsets = (
+        # Fieldset 1 : meta-info (titre, auteur…)
+       ('Général', {
+            'classes': ['collapse', ],
+            'fields': ('titre', 'auteur', 'categorie')
+        }),
+        # Fieldset 2 : contenu de l'article
+        ('Contenu de l\'article', {
+           'description': 'Le formulaire accepte les balises HTML. Utilisez-les à bon escient !',
+           'fields': ('contenu', )
+        }),
+    )
+
+admin.site.register(Categorie)
+admin.site.register(Article, ArticleAdmin)
